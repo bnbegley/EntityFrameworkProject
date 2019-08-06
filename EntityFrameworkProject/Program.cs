e09d7b62ba691566d8b86ef985cb614b05f9c6fd
brail33na11@gmail.com <brail33na11@gmail.com>
2019-08-06 16:24:10 -0400

    created database in Sql

--- EntityFrameworkProject/Program.cs
+++ EntityFrameworkProject/Program.cs
@@ -1,9 +1,31 @@
-﻿using System;
+﻿using EntityFrameworkProject.Models;
+using System;
+using System.Linq;
 
 namespace EntityFrameworkProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var context = new AppDbContext();
+
+            //var students = context.Students
+            //    .Where(s=> s.GPA >= 3.0)
+            //    .OrderByDescending(s => s.Lastname)
+            //    .ToArray();
+
+            // var students = context.Students.OrderBy(s => s.Firstname).ToList();
+
+            var studentAverageSAT = context.Students
+                .Average(s => s.SAT);
+
+            Console.WriteLine($"The average SAT score is {studentAverageSAT}");
+
+
+
+
+            //foreach(var student in studentAverageSAT) {
+            // Console.WriteLine($"{student.Firstname} {student.Lastname} {student.SAT}");
+            // }
         }
+
     }
 }
